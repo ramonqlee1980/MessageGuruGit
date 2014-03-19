600657114fc37edd6459067e4c1a16a63d4c0c50
ramonqlee1980 <ramonqlee1980@gmail.com>
2014-03-20 07:08:16 +0800

    搭建字体修改框架
    采用下拉菜单的方式，后续待功能完善后，考虑采用底部状态条变换的方式

--- MessageGuru/Card/RMCardEditorController.m
+++ MessageGuru/Card/RMCardEditorController.m
@@ -14,6 +14,7 @@
 #import "SWSnapshotStackView.h"
 #import "Flurry.h"
 #import "RMViewController+Aux.h"
+#import "REMenu.h"
 
 
 const NSUInteger kCellHeight = 44;
@@ -25,6 +26,7 @@ const NSUInteger kCellHeight = 44;
     CGPoint _inputViewOriginalPoint;
 }
 @property(nonatomic,retain)NSArray* backgroundImages;
+@property (retain, nonatomic) REMenu *menu;
 @end
 
 @implementation RMCardEditorController
@@ -131,6 +133,70 @@ const NSUInteger kCellHeight = 44;
     
     //flurry
     [Flurry logEvent:kEnterSMSCardUI];
+    
+    //add font menu
+//    self.view.backgroundColor = [UIColor colorWithWhite:0.902 alpha:1.000];
+//    self.navigationController.navigationBar.tintColor = [UIColor colorWithRed:0 green:179/255.0 blue:134/255.0 alpha:1];
+//    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Menu" style:UIBarButtonItemStyleBordered target:self action:@selector(showMenu)];
+    [self addFontButton];
+}
+-(void)addFontButton
+{
+    //这里创建一个圆角矩形的按钮
+    UIButton *button1 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    
+    //    能够定义的button类型有以下6种，
+    //    typedef enum {
+    //        UIButtonTypeCustom = 0,          自定义风格
+    //        UIButtonTypeRoundedRect,         圆角矩形
+    //        UIButtonTypeDetailDisclosure,    蓝色小箭头按钮，主要做详细说明用
+    //        UIButtonTypeInfoLight,           亮色感叹号
+    //        UIButtonTypeInfoDark,            暗色感叹号
+    //        UIButtonTypeContactAdd,          十字加号按钮
+    //    } UIButtonType;
+    
+    //给定button在view上的位置
+    button1.frame = CGRectMake(20, 20, 100, 50);
+    
+    //button背景色
+    button1.backgroundColor = [UIColor clearColor];
+    
+    //设置button填充图片
+    //[button1 setImage:[UIImage imageNamed:@"btng.png"] forState:UIControlStateNormal];
+    
+    //设置button标题
+    [button1 setTitle:@"字体" forState:UIControlStateNormal];
+    
+    /* forState: 这个参数的作用是定义按钮的文字或图片在何种状态下才会显现*/
+    //以下是几种状态
+    //    enum {
+    //        UIControlStateNormal       = 0,         常规状态显现
+    //        UIControlStateHighlighted  = 1 << 0,    高亮状态显现
+    //        UIControlStateDisabled     = 1 << 1,    禁用的状态才会显现
+    //        UIControlStateSelected     = 1 << 2,    选中状态
+    //        UIControlStateApplication  = 0x00FF0000, 当应用程序标志时
+    //        UIControlStateReserved     = 0xFF000000  为内部框架预留，可以不管他
+    //    };
+    
+    /*
+     * 默认情况下，当按钮高亮的情况下，图像的颜色会被画深一点，如果这下面的这个属性设置为no，
+     * 那么可以去掉这个功能
+     */
+    button1.adjustsImageWhenHighlighted = NO;
+    /*跟上面的情况一样，默认情况下，当按钮禁用的时候，图像会被画得深一点，设置NO可以取消设置*/
+    button1.adjustsImageWhenDisabled = NO;
+    /* 下面的这个属性设置为yes的状态下，按钮按下会发光*/
+    button1.showsTouchWhenHighlighted = YES;
+    
+    /* 给button添加事件，事件有很多种，我会单独开一篇博文介绍它们，下面这个时间的意思是
+     按下按钮，并且手指离开屏幕的时候触发这个事件，跟web中的click事件一样。
+     触发了这个事件以后，执行butClick:这个方法，addTarget:self 的意思是说，这个方法在本类中
+     也可以传入其他类的指针*/
+    [button1 addTarget:self action:@selector(showMenu) forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    //显示控件
+    self.navigationItem.titleView = button1;
 }
 
 -(void)viewWillAppear:(BOOL)animated
@@ -440,4 +506,74 @@ const NSUInteger kCellHeight = 44;
     
     return size;
 }
+
+#pragma mark REMenu
+
+- (void)showMenu
+{
+    if (_menu.isOpen)
+        return [_menu close];
+    
+    // Sample icons from http://icons8.com/download-free-icons-for-ios-tab-bar
+    //
+    
+    REMenuItem *homeItem = [[REMenuItem alloc] initWithTitle:@"Home"
+                                                    subtitle:@"Return to Home Screen"
+                                                       image:[UIImage imageNamed:@"Icon_Home"]
+                                            highlightedImage:nil
+                                                      action:^(REMenuItem *item) {
+                                                          NSLog(@"Item: %@", item);
+                                                      }];
+    
+    REMenuItem *exploreItem = [[REMenuItem alloc] initWithTitle:@"Explore"
+                                                       subtitle:@"Explore 47 additional options"
+                                                          image:[UIImage imageNamed:@"Icon_Explore"]
+                                               highlightedImage:nil
+                                                         action:^(REMenuItem *item) {
+                                                             NSLog(@"Item: %@", item);
+                                                         }];
+    
+    REMenuItem *activityItem = [[REMenuItem alloc] initWithTitle:@"Activity"
+                                                        subtitle:@"Perform 3 additional activities"
+                                                           image:[UIImage imageNamed:@"Icon_Activity"]
+                                                highlightedImage:nil
+                                                          action:^(REMenuItem *item) {
+                                                              NSLog(@"Item: %@", item);
+                                                          }];
+    
+    REMenuItem *profileItem = [[REMenuItem alloc] initWithTitle:@"Profile"
+                                                          image:[UIImage imageNamed:@"Icon_Profile"]
+                                               highlightedImage:nil
+                                                         action:^(REMenuItem *item) {
+                                                             NSLog(@"Item: %@", item);
+                                                         }];
+    
+    homeItem.tag = 0;
+    exploreItem.tag = 1;
+    activityItem.tag = 2;
+    profileItem.tag = 3;
+    
+    _menu = [[REMenu alloc] initWithItems:@[homeItem, exploreItem, activityItem, profileItem]];
+    _menu.cornerRadius = 4;
+    _menu.shadowColor = [UIColor blackColor];
+    _menu.shadowOffset = CGSizeMake(0, 1);
+    _menu.shadowOpacity = 1;
+    _menu.imageOffset = CGSizeMake(5, -1);
+    
+    [_menu showFromNavigationController:self.navigationController];
+}
+
+#pragma mark -
+#pragma mark Rotation
+
+- (BOOL)shouldAutorotate
+{
+    return YES;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation
+{
+    return YES;
+}
+
 @end
